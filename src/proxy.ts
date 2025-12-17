b7d9aae316c7e69c98d0d0cebf437e10e6d0fad2
Daniel Dao <danieldkdao@gmail.com>
2025-12-17 09:06:09 +0800

    setup arcjet env

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,4 +1,6 @@
+import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { env } from "./data/env/server";
 
 const isPublicRoute = createRouteMatcher([
   "/",
@@ -6,7 +8,32 @@ const isPublicRoute = createRouteMatcher([
   "/api/webhooks(.*)",
 ]);
 
-export default clerkMiddleware();
+const aj = arcjet({
+  key: env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:MONITOR", "CATEGORY:PREVIEW"],
+    }),
+    slidingWindow({
+      mode: "LIVE",
+      interval: "1m",
+      max: 100,
+    }),
+  ],
+});
+
+export default clerkMiddleware(async (auth, req) => {
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    return new Response(null, { status: 403 });
+  }
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
