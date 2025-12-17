066f6b43575551ebf312c9cd6f2f1f37b3fb22af
Daniel Dao <danieldkdao@gmail.com>
2025-12-17 08:54:09 +0800

    setup clerk

---
+++ src/services/clerk/components/clerk-provider.tsx
@@ -0,0 +1,6 @@
+import { ClerkProvider as OriginalClerkProvider } from "@clerk/nextjs";
+import type { ReactNode } from "react";
+
+export const ClerkProvider = ({ children }: { children: ReactNode }) => {
+  return <OriginalClerkProvider>{children}</OriginalClerkProvider>;
+};
